432f8ea9dc5e6530dbd67f34b18eddd11855317d
quincyocharles <quincyocharles@gmail.com>
2023-03-16 03:36:09 -0700

    hello printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * use the function printf
+ * return: 0
+ * compile without using -Wall gcc
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}	
