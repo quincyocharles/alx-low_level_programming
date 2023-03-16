1ccae9ba9e5fdb28e0edc064da77f7d7bdadf069
quincyocharles <quincyocharles@gmail.com>
2023-03-16 11:50:42 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,13 @@
 #include <unistd.h>
 /**
- * main - prints followed by a new line to the standard error
- * returns: 1
+ * main - prints and that piece of art is useful
+ *
+ * not allowed to use any functions listed in the NAME section
+ * Return: 1
 */
-
 int main(void)
 
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59)
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
