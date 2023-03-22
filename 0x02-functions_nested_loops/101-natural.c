8cc4cf89fddfbbf40f8673c507b56590502b0ae7
quincyocharles <quincyocharles@gmail.com>
2023-03-21 22:42:59 -0700

    program that computes and prints the sum of all the multiples of 3 or 5 below 1024

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - prints the sum of all multiples of 3 or 5 upto 1024
+ * Return: 0
+*/
+
+int main(void)
+{
+	int a, b = 0;
+
+	while (a < 1024)
+	{
+		if ((a % 3 == 0) || (a % 5 == 0))
+		{
+			b += a;
+		}
+		a++;
+	}
+	printf("%d\n", b);
+	return (0);
+}
