58c2cbdfe731762c13a900aafe1e48112f310dc5
quincyocharles <quincyocharles@gmail.com>
2023-04-05 00:40:38 -0700

    function that returns the natural square root of a number

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -2,21 +2,44 @@
 
 /**
  * _sqrt_recursion - returns the natural square root of a number
- * @n: the number to find the square root of
- * Return: the natural square root of n, or -1 if n does not have one
+ * @n: integer to find the square root of
+ * Return: the natural square root of the number if it exists, -1 otherwise
  */
 int _sqrt_recursion(int n)
 {
-	int t = 1, result = 1;
-
 	if (n < 0)
-	return (-1);
+		return (-1);
+
 	if (n == 0 || n == 1)
-	return (n);
-	while (result <= n)
+		return (n);
+
+	return (find_sqrt(1, n, n));
+}
+
+/**
+ * find_sqrt - finds the natural square root of a number
+ * @start: the start point of the search
+ * @end: the end point of the search
+ * @n: the original number to find the square root of
+ *
+ * Return: the natural square root of the number if it exists, -1 otherwise
+ */
+int find_sqrt(int start, int end, int n)
+{
+	int mid;
+
+	if (end >= start)
 	{
-	t++;
-	result = t * t;
+		mid = (start + end) / 2;
+
+		if (mid * mid == n)
+			return (mid);
+
+		if (mid * mid > n)
+			return (find_sqrt(start, mid - 1, n));
+
+		return (find_sqrt(mid + 1, end, n));
 	}
-	return (result == n ? t : _sqrt_recursion(n));
+
+	return (-1);
 }
