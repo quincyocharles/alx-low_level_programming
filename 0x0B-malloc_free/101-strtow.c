f3be112e7e1991264bf3b5f5afecdb51f1802be6
quincyocharles <quincyocharles@gmail.com>
2023-04-11 00:51:10 -0700

    function that splits a string into words

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,74 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * count_words - Counts the number of words in a string.
+ * @str: The string to be counted.
+ * Return: The number of words.
+ */
+static int count_words(char *str)
+{
+	int count = 0;
+	int in_word = 0;
+
+	while (*str)
+	{
+	if (*str == ' ' || *str == '\t' || *str == '\n')
+	in_word = 0;
+	else if (in_word == 0)
+	{
+	in_word = 1;
+	count++;
+	}
+	str++;
+	}
+	return (count);
+}
+
+/**
+ * strtow - Splits a string into words.
+ * @str: The string to be split.
+ * Return: A pointer to an array of strings (words), or NULL if str is NULL,
+ *         str is "", or memory allocation fails.
+ */
+char **strtow(char *str)
+{
+	int i, j, k, words;
+	char **matrix;
+
+	if (str == NULL || *str == '\0')
+	return (NULL);
+
+	words = count_words(str);
+	if (words == 0)
+	return (NULL);
+
+	matrix = malloc((words + 1) * sizeof(char *));
+	if (matrix == NULL)
+	return (NULL);
+
+	for (i = 0, j = 0; i < words; i++)
+	{
+	while (str[j] == ' ' || str[j] == '\t' || str[j] == '\n')
+	j++;
+
+	k = j;
+	while (str[k] && str[k] != ' ' && str[k] != '\t' && str[k] != '\n')
+	k++;
+
+	matrix[i] = malloc((k - j + 1) * sizeof(char));
+	if (matrix[i] == NULL)
+	{
+	for (i--; i >= 0; i--)
+	free(matrix[i]);
+	free(matrix);
+	return (NULL);
+	}
+	for (; j < k; j++)
+	matrix[i][j - k] = str[j];
+	matrix[i][j - k] = '\0';
+	}
+
+	matrix[i] = NULL;
+	return (matrix);
+}
