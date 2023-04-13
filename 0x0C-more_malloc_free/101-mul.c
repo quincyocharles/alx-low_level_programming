003095453948a1e5d8e9c1c0e4f988b1b29234b2
quincyocharles <quincyocharles@gmail.com>
2023-04-13 02:52:50 -0700

    program that multiplies two positive numbers

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -4,83 +4,123 @@
 #include <stdlib.h>
 
 /**
- * main - multiplies two positive numbers
- * @argc: argument count
- * @argv: argument vector
- * Return: 0 on success, 98 on failure
+ * _is_zero - determines if any number is zero
+ * @argv: argument vector.
+ *
+ * Return: no return.
  */
-int main(int argc, char **argv)
+void _is_zero(char *argv[])
 {
-	int i, j, len1, len2, res_len;
-	int *res;
+	int i, isn1 = 1, isn2 = 1;
 
-	/* Validate the number of arguments */
+	for (i = 0; argv[1][i]; i++)
+		if (argv[1][i] != '0')
+		{
+			isn1 = 0;
+			break;
+		}
+
+	for (i = 0; argv[2][i]; i++)
+		if (argv[2][i] != '0')
+		{
+			isn2 = 0;
+			break;
+		}
+
+	if (isn1 == 1 || isn2 == 1)
+	{
+		printf("0\n");
+		exit(0);
+	}
+}
+
+/**
+ * _initialize_array - set memery to zero in a new array
+ * @ar: char array.
+ * @lar: length of the char array.
+ *
+ * Return: pointer of a char array.
+ */
+char *_initialize_array(char *ar, int lar)
+{
+	int i = 0;
+
+	for (i = 0; i < lar; i++)
+		ar[i] = '0';
+	ar[lar] = '\0';
+	return (ar);
+}
+
+/**
+ * _checknum - determines length of the number
+ * and checks if number is in base 10.
+ * @argv: arguments vector.
+ * @n: row of the array.
+ *
+ * Return: length of the number.
+ */
+int _checknum(char *argv[], int n)
+{
+	int ln;
+
+	for (ln = 0; argv[n][ln]; ln++)
+		if (!isdigit(argv[n][ln]))
+		{
+			printf("Error\n");
+			exit(98);
+		}
+
+	return (ln);
+}
+
+/**
+ * main - Entry point.
+ * program that multiplies two positive numbers.
+ * @argc: number of arguments.
+ * @argv: arguments vector.
+ *
+ * Return: 0 - success.
+ */
+int main(int argc, char *argv[])
+{
+	int ln1, ln2, lnout, add, addl, i, j, k, ca;
+	char *nout;
+
 	if (argc != 3)
+		printf("Error\n"), exit(98);
+	ln1 = _checknum(argv, 1), ln2 = _checknum(argv, 2);
+	_is_zero(argv), lnout = ln1 + ln2, nout = malloc(lnout + 1);
+	if (nout == NULL)
+		printf("Error\n"), exit(98);
+	nout = _initialize_array(nout, lnout);
+	k = lnout - 1, i = ln1 - 1, j = ln2 - 1, ca = addl = 0;
+	for (; k >= 0; k--, i--)
 	{
-	printf("Error\n");
-	return (98);
+		if (i < 0)
+		{
+			if (addl > 0)
+			{
+				add = (nout[k] - '0') + addl;
+				if (add > 9)
+					nout[k - 1] = (add / 10) + '0';
+				nout[k] = (add % 10) + '0';
+			}
+			i = ln1 - 1, j--, addl = 0, ca++, k = lnout - (1 + ca);
+		}
+		if (j < 0)
+		{
+			if (nout[0] != '0')
+				break;
+			lnout--;
+			free(nout), nout = malloc(lnout + 1), nout = _initialize_array(nout, lnout);
+			k = lnout - 1, i = ln1 - 1, j = ln2 - 1, ca = addl = 0;
+		}
+		if (j >= 0)
+		{
+			add = ((argv[1][i] - '0') * (argv[2][j] - '0')) + (nout[k] - '0') + addl;
+			addl = add / 10, nout[k] = (add % 10) + '0';
+		}
 	}
-
-	/* Validate that num1 and num2 are composed of digits only */
-	for (i = 1; i < argc; i++)
-	{
-	for (j = 0; argv[i][j] != '\0'; j++)
-	{
-	if (argv[i][j] < '0' || argv[i][j] > '9')
-	{
-	printf("Error\n");
-	return (98);
-	}
-	}
-	}
-
-	/* Calculate the length of the two input numbers */
-	len1 = j - 1;
-	len2 = j - 1;
-
-	/* Allocate memory for the result array */
-	res_len = len1 + len2 + 1;
-	res = calloc(res_len, sizeof(int));
-	if (res == NULL)
-	{
-	printf("Error\n");
-	return (98);
-	}
-
-	/* Multiply the two input numbers */
-	for (i = len1; i >= 0; i--)
-	{
-	for (j = len2; j >= 0; j--)
-	{
-	res[i + j + 1] += (argv[1][i] - '0') * (argv[2][j] - '0');
-	}
-	}
-
-	/* Handle carries */
-	for (i = res_len - 1; i >= 0; i--)
-	{
-	if (res[i] > 9)
-	{
-	res[i - 1] += res[i] / 10;
-	res[i] %= 10;
-	}
-	}
-
-	/* Print the result */
-	i = 0;
-	while (i < res_len && res[i] == 0)
-	i++;
-	if (i == res_len)
-	printf("0\n");
-	else
-	{
-	for (; i < res_len; i++)
-	printf("%d", res[i]);
-	printf("\n");
-	}
-
-	/* Free memory */
-	free(res);
-
+	printf("%s\n", nout);
 	return (0);
 }
