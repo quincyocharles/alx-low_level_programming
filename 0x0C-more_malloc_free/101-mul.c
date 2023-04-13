e75b61c92661041dfbd0d55bddf52d2e0efbe189
quincyocharles <quincyocharles@gmail.com>
2023-04-13 03:33:07 -0700

    program that multiplies two positive numbers

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,113 +1,101 @@
 #include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+#define ERR_MSG "Error"
 
 /**
- * _isdigit - checks if a character is a digit
- * @c: the character to check
- * Return: 1 if c is a digit, 0 otherwise
+ * is_digit - checks if a string contains a non-digit char
+ * @s: string to be evaluated
+ *
+ * Return: 0 if a non-digit is found, 1 otherwise
  */
-int _isdigit(int c)
+int is_digit(char *s)
 {
-	return (c >= '0' && c <= '9');
-}
+	int i = 0;
 
-/**
- * _strlen - computes the length of a string
- * @s: the string to measure
- * Return: the length of s
- */
-size_t _strlen(const char *s)
-{
-	size_t n = 0;
-
-	while (*s++)
-		n++;
-
-	return (n);
-}
-
-/**
- * _puts - prints a string to stdout
- * @s: the string to print
- */
-void _puts(const char *s)
-{
-	write(STDOUT_FILENO, s, _strlen(s));
-}
-
-/**
- * mul - multiplies two positive numbers
- * @num1: the first number as a string
- * @num2: the second number as a string
- */
-void mul(const char *num1, const char *num2)
-{
-	size_t len1, len2, len;
-	int *result, i, j, carry, n1, n2, sum;
-
-	len1 = _strlen(num1);
-	len2 = _strlen(num2);
-	len = len1 + len2;
-	result = calloc(len, sizeof(*result));
-
-	if (!result)
-		exit(1);
-
-	for (i = len1 - 1; i >= 0; i--)
+	while (s[i])
 	{
-		carry = 0;
-		n1 = num1[i] - '0';
-
-		for (j = len2 - 1; j >= 0; j--)
-		{
-			n2 = num2[j] - '0';
-			sum = n1 * n2 + result[i + j + 1] + carry;
-			result[i + j + 1] = sum % 10;
-			carry = sum / 10;
-		}
-
-		result[i + j + 1] = carry;
-	}
-
-	i = 0;
-
-	while (i < len - 1 && result[i] == 0)
+		if (s[i] < '0' || s[i] > '9')
+			return (0);
 		i++;
-
-	for (; i < len; i++)
-		_putchar(result[i] + '0');
-
-	_putchar('\n');
-
-	free(result);
+	}
+	return (1);
 }
 
 /**
- * main - entry point
- * @argc: the number of command-line arguments
- * @argv: an array of command-line argument strings
- * Return: 0 on success, 98 on failure
+ * _strlen - returns the length of a string
+ * @s: string to evaluate
+ *
+ * Return: the length of the string
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * errors - handles errors for main
+ */
+void errors(void)
+{
+	printf("Error\n");
+	exit(98);
+}
+
+/**
+ * main - multiplies two positive numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: always 0 (Success)
  */
 int main(int argc, char *argv[])
 {
-	if (argc != 3)
-	{
-		_puts("Error\n");
-		return (98);
-	}
+	char *s1, *s2;
+	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
 
-	for (int i = 1; i < argc; i++)
+	s1 = argv[1], s2 = argv[2];
+	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
+		errors();
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
+	len = len1 + len2 + 1;
+	result = malloc(sizeof(int) * len);
+	if (!result)
+		return (1);
+	for (i = 0; i <= len1 + len2; i++)
+		result[i] = 0;
+	for (len1 = len1 - 1; len1 >= 0; len1--)
 	{
-		for (size_t j = 0; j < _strlen(argv[i]); j++)
+		digit1 = s1[len1] - '0';
+		carry = 0;
+		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
 		{
-			if (!_isdigit(argv[i][j]))
-			{
-				_puts("Error\n");
-				return (98);
-			}
+			digit2 = s2[len2] - '0';
+			carry += result[len1 + len2 + 1] + (digit1 * digit2);
+			result[len1 + len2 + 1] = carry % 10;
+			carry /= 10;
 		}
+		if (carry > 0)
+			result[len1 + len2 + 1] += carry;
 	}
-
-	mul(argv[1], argv[2]);
-
+	for (i = 0; i < len - 1; i++)
+	{
+		if (result[i])
+			a = 1;
+		if (a)
+			_putchar(result[i] + '0');
+	}
+	if (!a)
+		_putchar('0');
+	_putchar('\n');
+	free(result);
 	return (0);
 }
