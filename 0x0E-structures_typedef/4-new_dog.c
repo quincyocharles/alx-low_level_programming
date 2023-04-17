cc141f08945a16040b341a226bebd076d996df96
quincyocharles <quincyocharles@gmail.com>
2023-04-17 05:23:25 -0700

    function that creates a new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,76 +1,52 @@
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "dog.h"
 
 /**
- * new_dog - creates a new dog with the given name, age, and owner
+ * new_dog - creates a new dog struct
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
  *
- * @name: the name of the dog
- * @age: the age of the dog
- * @owner: the owner of the dog
- *
- * Return: a pointer to the new dog, or NULL if malloc fails
+ * Return: pointer to the new dog (Success), NULL otherwise
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	/* Allocate memory for the new dog */
-	dog_t *new_dog = malloc(sizeof(dog_t));
+	size_t name_len, owner_len;
+		dog_t *new_dog;
+
+	if (name == NULL || owner == NULL)
+		return (NULL);
+
+	name_len = strlen(name);
+	owner_len = strlen(owner);
+
+	new_dog = malloc(sizeof(dog_t));
 
 	if (new_dog == NULL)
-	return (NULL);
+		return (NULL);
 
-	/* Allocate memory and copy the name */
-	char *new_name = malloc(sizeof(char) * (_strlen(name) + 1));
+	new_dog->name = malloc(sizeof(char) * (name_len + 1));
 
-	if (new_name == NULL)
+	if (new_dog->name == NULL)
 	{
-	free(new_dog);
-	return (NULL);
+		free(new_dog);
+		return (NULL);
 	}
-	_strcpy(new_name, name);
 
-	/* Allocate memory and copy the owner */
-	char *new_owner = malloc(sizeof(char) * (_strlen(owner) + 1));
+	new_dog->owner = malloc(sizeof(char) * (owner_len + 1));
 
-	if (new_owner == NULL)
+	if (new_dog->owner == NULL)
 	{
-	free(new_name);
-	free(new_dog);
-	return (NULL);
+		free(new_dog->name);
+		free(new_dog);
+		return (NULL);
 	}
-	_strcpy(new_owner, owner);
 
-	/* Set the dog's attributes and return it */
-	new_dog->name = new_name;
+	strncpy(new_dog->name, name, name_len + 1);
+	strncpy(new_dog->owner, owner, owner_len + 1);
 	new_dog->age = age;
-	new_dog->owner = new_owner;
+
 	return (new_dog);
-}
-
-/**
- * _strlen - returns the length of a string
- *
- * @s: the string to measure
- *
- * Return: the length of the string
- */
-int _strlen(char *s)
-{
-	int len = 0;
-
-	while (*(s++))
-	len++;
-	return (len);
-}
-
-/**
- * _strcpy - copies a string from src to dest
- *
- * @dest: the destination buffer
- * @src: the source string
- */
-void _strcpy(char *dest, char *src)
-{
-	while (*src)
-	*(dest++) = *(src++);
-	*dest = ('\0');
 }
